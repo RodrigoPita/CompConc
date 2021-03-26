b522efde89327a9ac5ce0739a75a15bd5608f230
Rodrigo Pita <41768439+RodrigoPita@users.noreply.github.com>
2021-03-26 16:05:34 -0300

    Add files via upload
    Aula 4

--- main.c
+++ main.c
@@ -3,9 +3,12 @@
 
 #define NTHREADS 10
 
+int vetor[NTHREADS];
+
 //funcao que a thread ira executar
 void * tarefa (void * arg) {
     int ident = * (int *) arg;
+    vetor[ident-1] = ident;
     printf("Ola, sou a thread %d!\n", ident);
     pthread_exit(NULL);
 }
@@ -20,11 +23,20 @@ int main(void) {
         if (pthread_create(&tid[i-1], NULL, tarefa, (void *)&ident[i-1]))
             printf("ERRO -- pthread_create\n");   
     }
-        
+    //espera as threads terminarem
+    for(int i = 0; i < NTHREADS; i++) {
+        if (pthread_join(tid[i], NULL))
+            printf("ERRO -- pthread_create\n");
+    }
+    //imprime o vetor de identificadores
+    for(int i = 0; i < NTHREADS; i++)
+        printf("%d ", vetor[i]);
+    printf("\n");
+    
     //imprime a mensagem de boas vindas
     printf("Ola, sou a thread principal!\n");
     
     //desvincula o termino da main do termino do programa
-    pthread_exit(NULL);
+    //pthread_exit(NULL);
     return 0;
 }
