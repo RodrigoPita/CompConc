008e1d7f16831278e9cc6d689fd3bf68e10dbf21
Rodrigo Pita <41768439+RodrigoPita@users.noreply.github.com>
2021-03-31 11:20:12 -0300

    Update semana2.c
    aula3

--- semana2.c
+++ semana2.c
@@ -2,13 +2,31 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 float *mat; //matriz de entrada
 float *vet; //vetor de entrada
 float *saida; //vetor de saida
 
+typedef struct{
+    int id; //identificador do elemento que a thread ira processar
+    int dim; //dimensao das estruturas de entrada
+} tArgs;
+
+//funcao que as threads executarao
+void * tarefa(void *arg) {
+    tArgs *args = (tArgs*) arg;
+    for(int j = 0; j < args->dim; j++)
+        saida[args->id] += mat[(args->id)*(args->dim) + j] * vet[j];
+    pthread_exit(NULL);
+}
+
+//fluxo principal
 int main(int argc, char* argv[]) {
     int dim; //dimensao da matriz de entrada
+    pthread_t *tid; //identificadores das treads no sistema
+    tArgs *args; //identificadores locais das threads e dimensao
+    
     //leitura e avaliacao dos parametros de entrada
     if(argc < 2) {
         printf("Digite: %s <dimensao da matriz>\n", argv[0]);
@@ -33,12 +51,24 @@ int main(int argc, char* argv[]) {
         saida[i] = 0;
     }
     //multiplicacao da matriz pelo vetor
-    for(int i = 0; i < dim; i++) {
-        for(int j = 0; j < dim; j++) {
-            saida[i] += mat[i*dim + j]*vet[j];
-        }
-    }
+    //alocacao das estruturas
+    tid = (pthread_t*) malloc(sizeof(pthread_t)*dim);
+    if(tid == NULL) {puts("ERRO--malloc"); return 2;}
+    args = (tArgs*) malloc(sizeof(tArgs)*dim);
+    if(args == NULL) {puts("ERRO--malloc"); return 2;}
     
+    //criacao das threads
+    for(int i = 0; i < dim; i++) {
+        (args+i)->id = i;
+        (args+i)->dim = dim;
+        if(pthread_create(tid+i, NULL, tarefa, (void*) (args+i))) {
+            puts("ERRO--pthread_create"); return 3;
+        }
+    }    
+    //espera pelo termino das threads
+    for(int i = 0; i < dim; i++)
+        pthread_join(*(tid+i), NULL);
+        
     //exibicao dos resultados
     puts("Matriz de entrada:");
     for(int i = 0; i < dim; i++) {
@@ -59,6 +89,8 @@ int main(int argc, char* argv[]) {
     free(mat);
     free(vet);
     free(saida);
+    free(args);
+    free(tid);
     
     return 0;
 }
