8c731d3bf0cd9b4d1bf2212a2c469a8aef0bc99c
Lorena Mamede <loremamede@gmail.com>
2021-05-24 21:54:07 -0300

    [Trabalho 2] Criando mecanismo de teste via log

--- trab2/PadraoEscritorLeitor.c
+++ trab2/PadraoEscritorLeitor.c
@@ -2,69 +2,98 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <string.h>
 
 //variaveis do problema
-int leitoras_ativas=0; //contador de threads lendo
-int escritoras_ativas=0; //contador de threads escrevendo
-int escritoras_fila=0;
+FILE *fd;                  //arquivo de log
+int leitoras_ativas = 0;   //contador de threads lendo
+int escritoras_ativas = 0; //contador de threads escrevendo
+int escritoras_fila = 0;
 
 //variaveis para sincronizacao
 pthread_mutex_t mutex;
 pthread_cond_t cond_leit, cond_escr;
 
+void criarLog()
+{
+  fd = fopen("PadraoEscritorLeitor.py", "w+");
+  fprintf(fd, "%s", "import teste\n");
+  fprintf(fd, "%s", "le = teste.LE()\n");
+  fclose(fd);
+}
+
+void escreverNoLog(const char * string, int id)
+{
+  fd = fopen("PadraoEscritorLeitor.py", "a+");
+  fprintf(fd, string, id);
+  fclose(fd);
+}
 //entrada leitura
-void entrarLeitora(int id) {
-   pthread_mutex_lock(&mutex);
+void entrarLeitora(int id)
+{
+  pthread_mutex_lock(&mutex);
   //printf("L[%d] quer ler\n", id);
-   while(escritoras_ativas > 0 || escritoras_fila > 0) {
-     //printf("L[%d] bloqueou\n", id);
-     if(escritoras_fila > 0){
-       pthread_cond_signal(&cond_escr);
-     }
-     pthread_cond_wait(&cond_leit, &mutex);
-     //printf("L[%d] desbloqueou\n", id);
-   }
-   leitoras_ativas++;
-   pthread_mutex_unlock(&mutex);
+  while (escritoras_ativas > 0 || escritoras_fila > 0)
+  {
+    escreverNoLog("le.leitorBloqueado(%d)\n", id);
+    if (escritoras_fila > 0)
+    {
+      pthread_cond_signal(&cond_escr);
+    }
+    pthread_cond_wait(&cond_leit, &mutex);
+    //printf("L[%d] desbloqueou\n", id);
+  }
+  leitoras_ativas++;
+  escreverNoLog("le.leitorLendo(%d)\n", id);
+  pthread_mutex_unlock(&mutex);
 }
 
 //saida leitura
-void sairLeitora (int id) {
-   pthread_mutex_lock(&mutex);
-   //printf("L[%d] terminou de ler\n", id);
-   leitoras_ativas--;
-   if(leitoras_ativas==0) pthread_cond_signal(&cond_escr);
-   pthread_mutex_unlock(&mutex);
+void sairLeitora(int id)
+{
+  pthread_mutex_lock(&mutex);
+  escreverNoLog("le.leitorSaindo(%d)\n", id);
+  leitoras_ativas--;
+  if (leitoras_ativas == 0)
+    pthread_cond_signal(&cond_escr);
+  pthread_mutex_unlock(&mutex);
 }
 
 //entrada escrita
-void entrarEscritora (int id) {
-   pthread_mutex_lock(&mutex);
-   //printf("E[%d] quer escrever\n", id);
-   while((leitoras_ativas > 0) || (escritoras_ativas > 0)) {
-     //printf("E[%d] bloqueou\n", id);
-     escritoras_fila++;
-     pthread_cond_wait(&cond_escr, &mutex);
-     escritoras_fila--;
-     //printf("E[%d] desbloqueou\n", id);
-   }
-   escritoras_ativas++;
-   pthread_mutex_unlock(&mutex);
+void entrarEscritora(int id)
+{
+  pthread_mutex_lock(&mutex);
+  //printf("E[%d] quer escrever\n", id);
+  while ((leitoras_ativas > 0) || (escritoras_ativas > 0))
+  {
+    escreverNoLog("le.escritorBloqueado(%d)\n", id);
+    escritoras_fila++;
+    pthread_cond_wait(&cond_escr, &mutex);
+    escritoras_fila--;
+    escreverNoLog("le.escritorDesbloqueado(%d)\n", id);
+  }
+  escritoras_ativas++;
+  escreverNoLog("le.escritorEscrevendo(%d)\n", id);
+  pthread_mutex_unlock(&mutex);
 }
 
 //saida escrita
-void sairEscritora (int id) {
+void sairEscritora(int id)
+{
   pthread_mutex_lock(&mutex);
-  //printf("E[%d] terminou de escrever\n", id);
+  escreverNoLog("le.escritorSaindo(%d)\n", id);
+
   escritoras_ativas--;
   pthread_cond_signal(&cond_escr);
   pthread_cond_broadcast(&cond_leit);
   pthread_mutex_unlock(&mutex);
 }
 
-void prepararSincronizacao(){
-  pthread_mutex_init(&mutex, NULL);
-	pthread_cond_init(&cond_leit, NULL);
-	pthread_cond_init(&cond_escr, NULL);
-}
+void prepararSincronizacao()
+{
+  criarLog();
 
+  pthread_mutex_init(&mutex, NULL);
+  pthread_cond_init(&cond_leit, NULL);
+  pthread_cond_init(&cond_escr, NULL);
+}
